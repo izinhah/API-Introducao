c7f2fa0e3b19a6d3681631324c4c1a616e0f0048
izinhah <133233107+izinhah@users.noreply.github.com>
2023-08-04 11:36:38 +0000

    isabel

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import express from "express";      // Requisição do pacote do express
+//index.js
+import dotenv from "dotenv";
+import express from "express";      // Requisição do pacote do express 
+import { selectUsuarios } from "./bd.js";
+dotenv.config();
+
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -11,4 +17,14 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+}); 
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
