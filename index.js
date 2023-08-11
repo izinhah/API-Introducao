60ff83704ff7b9e4cb94d958ef6d644e869b9f79
izinhah <133233107+izinhah@users.noreply.github.com>
2023-08-11 11:39:53 +0000

    iSa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express 
-import { selectUsuarios, selectUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 dotenv.config();
 
@@ -39,6 +39,19 @@ app.get("/usuario/:id", async (req, res) => {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+}); 
+
+app.use(express.json()); 
+
+//index.js
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
