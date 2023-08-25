6d0fc472fa01bbb2dd15bd34522fc8ef1b54ea17
izinhah <133233107+izinhah@users.noreply.github.com>
2023-08-25 11:19:10 +0000

    isa

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import roteadorUsuario from "./routes/usuario.js";
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(roteadorUsuario);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",
+  });
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+
+
